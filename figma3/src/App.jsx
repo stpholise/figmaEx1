7564ca1a680d5ce85df8b11af82448ab7b4411ec
stpholise <stpholise@gmail.com>
2024-10-11 05:17:49 +0100

    a new text added to app.jsx

--- figma3/src/App.jsx
+++ figma3/src/App.jsx
@@ -25,6 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <p> lets seee what this will do in the git </p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
